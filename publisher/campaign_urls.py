0a431b062452438da8501efe4eba20f7a8ad023b
Niomi Samani <94210866+niomisamani@users.noreply.github.com>
2026-01-19 10:11:01 +0530

    Add campaign URLs for publisher views

---
+++ publisher/campaign_urls.py
@@ -0,0 +1,39 @@
+from django.urls import path
+
+from . import campaign_views
+
+app_name = "campaign_publisher"
+
+urlpatterns = [
+    path(
+        "publisher-landing-page/",
+        campaign_views.publisher_landing_page,
+        name="publisher_landing_page",
+    ),
+    path(
+        "add-campaign-details/",
+        campaign_views.add_campaign_details,
+        name="add_campaign_details",
+    ),
+    path(
+        "campaigns/",
+        campaign_views.campaign_list,
+        name="campaign_list",
+    ),
+    path(
+        "campaigns/<str:campaign_id>/edit/",
+        campaign_views.edit_campaign_details,
+        name="edit_campaign_details",
+    ),
+    # APIs for search + selection expansion
+    path(
+        "publisher-api/search/",
+        campaign_views.api_search_catalog,
+        name="api_search_catalog",
+    ),
+    path(
+        "publisher-api/expand-selection/",
+        campaign_views.api_expand_selection,
+        name="api_expand_selection",
+    ),
+]
